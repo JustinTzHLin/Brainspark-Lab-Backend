ffa18ff0c44fdcf0829e153fa3e748516b13e381
Justin Tzu-Hung Lin <hozion612356@gmail.com>
2024-09-01 22:21:24 -0700

    Fix reading environment variables

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import userRoute from './routes/userRoute.js';
 import quizRoute from './routes/quizRoute.js';
